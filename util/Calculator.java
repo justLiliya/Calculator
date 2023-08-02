930ccbe8bab70d28123ed8d4dce8530b44cc3d04
Lil <liliapaper@gmail.com>
2023-08-02 20:46:12 +0300

    Third commit - третий коммит

--- util/Calculator.java
+++ util/Calculator.java
@@ -4,9 +4,6 @@ import static java.lang.Math.max;
 import static java.lang.Math.min;
 
 public class Calculator {
-    //Внутри Calculator Должны быть реализованы 3 метода: сумма, разность,
-    // произведение. (с английскими названиями. названия у методов должны быть
-    // словами-глаголами) Каждый из этих методов должен принимать 2 числа в качестве параметров и возвращать результат.
 
     public static int sum(int firstNum, int secondNum) {
         int result = firstNum + secondNum;
