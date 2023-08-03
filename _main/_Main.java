08ec0df07d33f091a57c7d1252bd2d66c0cbd8c6
Lil <liliapaper@gmail.com>
2023-08-03 11:28:20 +0300

    Убрала статические импорты

--- _main/_Main.java
+++ _main/_Main.java
@@ -1,15 +1,15 @@
 package ru.khlebnikova.calcapp._main;
 
-import static ru.khlebnikova.calcapp.service.ResultWriterService.typeResult;
-import static ru.khlebnikova.calcapp.util.Calculator.*;
+import ru.khlebnikova.calcapp.service.ResultWriterService;
+import ru.khlebnikova.calcapp.util.Calculator;
 
 public class _Main {
 
     public static void main(String[] args) {
 
-        typeResult(15, 2, sum(15, 2), "sum");
-        typeResult(15, 25, difference(15, 25), "difference");
-        typeResult(15, 2, multiplication(15, 2), "multiplication");
+        ResultWriterService.typeResult(15, 2, Calculator.sum(15, 2), "sum");
+        ResultWriterService.typeResult(15, 25, Calculator.difference(15, 25), "difference");
+        ResultWriterService.typeResult(15, 2, Calculator.multiplication(15, 2), "multiplication");
 
     }
 }
