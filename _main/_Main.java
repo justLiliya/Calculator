349b573ccf3057566d82992ba6dc6293e8b92caf
Lil <liliapaper@gmail.com>
2023-08-03 13:37:22 +0300

    removed indents

--- _main/_Main.java
+++ _main/_Main.java
@@ -6,10 +6,8 @@ import ru.khlebnikova.calcapp.util.Calculator;
 public class _Main {
 
     public static void main(String[] args) {
-
         ResultWriterService.typeResult(15, 2, Calculator.sum(15, 2), "sum");
         ResultWriterService.typeResult(15, 25, Calculator.difference(15, 25), "difference");
         ResultWriterService.typeResult(15, 2, Calculator.multiplication(15, 2), "multiplication");
-
     }
 }
